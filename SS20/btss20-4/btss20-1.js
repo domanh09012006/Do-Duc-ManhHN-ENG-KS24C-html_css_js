64b3dc9788f7dd51d794053bd42f4db99fe80f24
domanh09012006 <doducmanh09012006@gmail.com>
2025-03-05 07:54:03 +0700

    ss20

--- SS20/btss20-4/btss20-1.js
+++ SS20/btss20-4/btss20-1.js
@@ -1,19 +1,14 @@
 let str = prompt("Nhập vào 1 chuỗi bất kì: ")
 let character = prompt("Nhập vào kí tự cần tìm kiếm trong chuỗi vừa nhập: ")
 let Length = str.length
-for(let i=0; i<Length; i++ ){
-    // if(str.lastIndexOf(character)>0){
-    //     alert("Từ tìm kiếm có tồn tại")
-    //     break;
-    // }else{
-    //     alert("từ tìm kiếm ko tồn tại")
-    //     break;
-    // }
-    if(str[i] == character){
-        alert("Từ tìm kiếm có tồn tại!")
-        break;
-    }else{
-        alert("Từ tìm kiếm không tồn tại!")
-        break;
+let check = 1
+for (let i = 0; i < Length; i++) {
+    if (str[i] == character) {
+        check++;
     }
+}
+if (check > 1) {
+    alert("Ton taij tu can tim kiem")
+} else {
+    alert("Khong ton tai tu can tim kiem")
 }
